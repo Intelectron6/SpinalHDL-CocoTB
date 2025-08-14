d01edb5ccbd1f9e5dfe9ee6891372188c5b49396
Dhruva Hegde <53150130+Intelectron6@users.noreply.github.com>
2025-08-14 20:47:27 +0530

    Better indentation.

--- Matrix-Determinant/MatDet_IP.scala
+++ Matrix-Determinant/MatDet_IP.scala
@@ -41,80 +41,80 @@ case class MatDet_IP() extends Component
 		c := 0
 		d := 0
 		e := 0
-
+		
 		//stateA - FSM enters here on reset or when computation is done
-    val stateA : State = new State with EntryPoint
-    {
-        whenIsActive
-        {
-          //regs10 = 1 indicates that the computation has to start or is done
-          when (regs(10) === S(1))
-          {
-            when (indreg === U(1)) //indreg = 1 indicates that the computation is done
-            {
-              regs(9) := result.resized //store result in regs(9)
-              regs(10) := S(0)          //make regs(10) = 0 in order to not start new computation again
-              regs(11) := S(2)	      //make regs(11) = 2 to show that the result is valid
-            }
-            .otherwise //indreg = 0 indicates that the computation has to start 
-            {
-              regs(9) := S(0)           //store 0 in regs(9) just to clear any previous value
-              regs(11) := S(1)          //make regs(11) = 1 to show that the result is not valid and computation is ongoing
-              goto(stateB)              //go to stateB to start the computation
-            }
-          }
-          comp_en := False //no computation is happening in this cycle (note that comp_en is a signal, not a register)
-        }
-    }
-    
-    //stateB - first cycle of computation
-    val stateB : State = new State 
-    {
-      whenIsActive //store appropriate values in registers a, b, c, d, e; enable computation and go to stateC
-      {
-        goto(stateC)
-        a := regs(0).resized
-        b := regs(4).resized 
-        c := regs(8).resized
-        d := regs(7).resized
-        e := regs(5).resized
-        comp_en := True
-      }
-    }
-    
-    //stateC - second cycle of computation
-    val stateC : State = new State 
-    {
-        whenIsActive //store appropriate values in registers a, b, c, d, e; enable computation and go to stateD
-        {
-          goto(stateD)
-          a := regs(1).resized
-          b := regs(6).resized 
-          c := regs(5).resized
-          d := regs(8).resized
-          e := regs(3).resized 
-          comp_en := True
-        }
-    }
-
-    //stateD - third cycle of computation
-    val stateD : State = new State 
-    {
-        whenIsActive //store appropriate values in registers a, b, c, d, e; enable computation and go to stateD
-        {
-          goto(stateA)
-          a := regs(2).resized
-          b := regs(3).resized
-          c := regs(7).resized 
-          d := regs(4).resized
-          e := regs(6).resized
-          indreg := U(1)
-          comp_en := True
-        }
-    }
+		val stateA : State = new State with EntryPoint
+		{
+			whenIsActive
+			{
+				//regs10 = 1 indicates that the computation has to start or is done
+				when (regs(10) === S(1))
+				{
+					when (indreg === U(1)) //indreg = 1 indicates that the computation is done
+					{
+						regs(9) := result.resized //store result in regs(9)
+						regs(10) := S(0)          //make regs(10) = 0 in order to not start new computation again
+						regs(11) := S(2)	      //make regs(11) = 2 to show that the result is valid
+					}
+					.otherwise //indreg = 0 indicates that the computation has to start 
+					{
+						regs(9) := S(0)           //store 0 in regs(9) just to clear any previous value
+						regs(11) := S(1)          //make regs(11) = 1 to show that the result is not valid and computation is ongoing
+						goto(stateB)              //go to stateB to start the computation
+					}
+				}
+				comp_en := False //no computation is happening in this cycle (note that comp_en is a signal, not a register)
+			}
+		}
+			
+		//stateB - first cycle of computation
+		val stateB : State = new State 
+		{
+			whenIsActive //store appropriate values in registers a, b, c, d, e; enable computation and go to stateC
+			{
+				goto(stateC)
+				a := regs(0).resized
+				b := regs(4).resized 
+				c := regs(8).resized
+				d := regs(7).resized
+				e := regs(5).resized
+				comp_en := True
+			}
+		}
+			
+		//stateC - second cycle of computation
+		val stateC : State = new State 
+		{
+			whenIsActive //store appropriate values in registers a, b, c, d, e; enable computation and go to stateD
+			{
+				  goto(stateD)
+				  a := regs(1).resized
+				  b := regs(6).resized 
+				  c := regs(5).resized
+				  d := regs(8).resized
+				  e := regs(3).resized 
+				  comp_en := True
+			}
+		}
+			
+		//stateD - third cycle of computation
+		val stateD : State = new State 
+		{
+			whenIsActive //store appropriate values in registers a, b, c, d, e; enable computation and go to stateD
+			{
+				  goto(stateA)
+				  a := regs(2).resized
+				  b := regs(3).resized
+				  c := regs(7).resized 
+				  d := regs(4).resized
+				  e := regs(6).resized
+				  indreg := U(1)
+				  comp_en := True
+			}
+		}
 	}
-
-  //Data path to compute determinant
+		
+	//Data path to compute determinant
 	when (comp_en === True)
 	{
 		result := (result + (a*(b*c - d*e)))
